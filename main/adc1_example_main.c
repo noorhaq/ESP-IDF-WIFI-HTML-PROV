10aac644493a69d1f7659bcf923bad5ce2e81edd
noorhaq <noorhaq55@gmail.com>
2022-05-19 03:45:55 +0500

    Final release

--- main/adc1_example_main.c
+++ main/adc1_example_main.c
@@ -13,15 +13,127 @@
 #include "driver/gpio.h"
 #include <esp_http_server.h>
 
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include <math.h>
+
 #include <sys/param.h>
 #include "esp_netif.h"
 
 #include "lwip/err.h"
+#include "lwip/sockets.h"
 #include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
 
-#define EXAMPLE_ESP_WIFI_SSID      ""
-#define EXAMPLE_ESP_WIFI_PASS      ""
+#include "esp_tls.h"
+#include "esp_crt_bundle.h"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  5
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+
+/************************Hardware Related Macros************************************/
+#define         MQ_PIN                       (0)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+ 
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation
+ 
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG                      (0)
+#define         GAS_CO                       (1)
+#define         GAS_SMOKE                    (2)
+
+static const char *web_urls = "http://iot-vfbcooifca-uc.a.run.app/alerts";
+/*****************************Globals***********************************************/
+float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59) 
+float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15) 
+float           SmokeCurve[3] ={2.3,0.53,-0.44};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
+float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
+
+
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve);
+int  MQGetGasPercentage(float rs_ro_ratio, int gas_id);
+float MQRead();
+float MQCalibration();
+float MQResistanceCalculation(int raw_adc);
+static int AnalogRead();
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void delay(uint32_t time)
+{
+    vTaskDelay(pdMS_TO_TICKS(time));
+}
+
+
+static esp_adc_cal_characteristics_t *adc_chars;
+#if CONFIG_IDF_TARGET_ESP32
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+#elif CONFIG_IDF_TARGET_ESP32S2
+static const adc_channel_t channel = ADC_CHANNEL_6;     // GPIO7 if ADC1, GPIO17 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
+#endif
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+
+static void check_efuse(void)
+{
+#if CONFIG_IDF_TARGET_ESP32
+    //Check if TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+#elif CONFIG_IDF_TARGET_ESP32S2
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("Cannot retrieve eFuse Two Point calibration values. Default calibration values will be used.\n");
+    }
+#else
+#error "This example is configured for ESP32/ESP32S2."
+#endif
+}
+
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
 
 /* FreeRTOS event group to signal when we are connected*/
 
@@ -230,7 +342,7 @@ static httpd_handle_t start_webserver(void)
 
 void wifi_init_softap()
 {
-    size_t required_size;
+    size_t required_size = 0;
 
     nvs_handle_t my_handle;
     esp_err_t  err = nvs_open("storage", NVS_READWRITE, &my_handle);
@@ -248,9 +360,11 @@ void wifi_init_softap()
                     case ESP_OK:
                         printf("Done\n");
                         cJSON *root = cJSON_Parse(BUFF);
-                        sprintf(__SSID, "%s", cJSON_GetObjectItem(root, "ssid")->valuestring);
-                        sprintf(__PWD, "%s", cJSON_GetObjectItem(root, "pwd")->valuestring);
-
+                        if(cJSON_GetObjectItem(root, "ssid"))
+                       {
+                            sprintf(__SSID, "%s", cJSON_GetObjectItem(root, "ssid")->valuestring);
+                            sprintf(__PWD, "%s", cJSON_GetObjectItem(root, "pwd")->valuestring);
+                       }
                         break;
                     case ESP_ERR_NVS_NOT_FOUND:
                         printf("The value is not initialized yet!\n");
@@ -359,6 +473,162 @@ void wifi_init_softap()
     start_webserver();
 }
 
+static int AnalogRead()
+{
+    int adc_reading = 0;
+            if (unit == ADC_UNIT_1) {
+                adc_reading = adc1_get_raw((adc1_channel_t)channel);
+            } else {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, width, &raw);
+                adc_reading = raw;
+                }
+        // printf("Raw: %d\n", adc_reading);
+        adc_reading = map(adc_reading, 0, 4095, 0, 1023);
+        return adc_reading;
+}
+
+  /****************** MQResistanceCalculation **************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+ 
+/***************************** MQCalibration **************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration()
+{
+  int i;
+  float val=0;
+ 
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(AnalogRead());
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+ 
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                        //according to the chart in the datasheet 
+ 
+  return val; 
+}
+/*****************************  MQRead *******************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead()
+{
+  int i;
+  float rs=0;
+ 
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(AnalogRead());
+    delay(READ_SAMPLE_INTERVAL);
+  }
+ 
+  rs = rs/READ_SAMPLE_TIMES;
+ 
+  return rs;  
+}
+ 
+/*****************************  MQGetGasPercentage ********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which 
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_LPG ) {
+     return MQGetPercentage(rs_ro_ratio,LPGCurve);
+  } else if ( gas_id == GAS_CO ) {
+     return MQGetPercentage(rs_ro_ratio,COCurve);
+  } else if ( gas_id == GAS_SMOKE ) {
+     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+  }    
+ 
+  return 0;
+}
+ 
+/*****************************  MQGetPercentage ********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
+         value.
+************************************************************************************/ 
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
+
+//====-------------------------------------------
+
+static void ADC_READ(void *pvParameters)
+{
+     check_efuse();
+
+    //Configure ADC
+    if (unit == ADC_UNIT_1) {
+        adc1_config_width(width);
+        adc1_config_channel_atten(channel, atten);
+    } else {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    while (1) {
+        int adc_reading = 0;
+        //Multisampling
+        adc_reading = AnalogRead();
+        printf("Raw: %d      LPG = %d ppm\n", adc_reading, MQGetGasPercentage(MQRead()/Ro,GAS_LPG));
+        printf("CO: %d ppm  SMOKE = %d ppm\n", MQGetGasPercentage(MQRead()/Ro,GAS_CO), MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_SMOKE));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        //------------------HTTP request
+       /* if(GOT_IP) {
+            esp_tls_cfg_t cfg = {
+                    .crt_bundle_attach = esp_crt_bundle_attach,
+                };
+
+                struct esp_tls *tls = esp_tls_conn_http_new(web_urls, &cfg);
+
+                if (tls != NULL) {
+                    ESP_LOGI(TAG, "Connection established to %s", web_urls);
+                } else {
+                    ESP_LOGE(TAG, "Could not connect to %s", web_urls);
+                }
+
+                esp_tls_conn_delete(tls);
+        } */
+    }
+
+}
+//============================================================
+//====================APP_MAIN
+//==========================================================
 void app_main()
 {
     esp_err_t ret = nvs_flash_init();
@@ -371,7 +641,9 @@ void app_main()
 
     ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+     wifi_init_softap();
 
-    wifi_init_softap();
+    xTaskCreate(&ADC_READ, "ADC_READ", 8192, NULL, 5, NULL); 
+    
 }
+

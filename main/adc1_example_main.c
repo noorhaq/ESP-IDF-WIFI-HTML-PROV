fa8a42cbf3a003b0e7ca01d741f919c2dbbeb2c5
noorhaq <noorhaq55@gmail.com>
2022-05-19 01:38:52 +0500

    Working HTML provisioning

--- main/adc1_example_main.c
+++ main/adc1_example_main.c
@@ -19,12 +19,25 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+#define EXAMPLE_ESP_WIFI_SSID      ""
+#define EXAMPLE_ESP_WIFI_PASS      ""
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
+
+/* FreeRTOS event group to signal when we are connected*/
 
 static EventGroupHandle_t s_wifi_event_group;
 
 static EventGroupHandle_t wifi_event_group;
 
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
 const int CONNECTED_BIT = BIT0;
+static int s_retry_num = 0;
+
 
 static const char *TAG = "ESP32_Server";
 
@@ -51,23 +64,18 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "wifi_event_handler wifi_event_handler wifi_event_handler");
 }
 
-static esp_err_t event_handler(void *ctx, system_event_t *event)
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
-    switch (event->event_id)
-    {
-    case SYSTEM_EVENT_STA_START:
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP ");
-        ESP_LOGI(TAG, "Login Success");
-        GOT_IP = true;
-        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-
-        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED %d ", esp_wifi_connect());
-        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         if (GOT_IP == false)
         {
             ESP_LOGI(TAG, "Sorry wrong PSW");
@@ -76,12 +84,18 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
             esp_sleep_enable_timer_wakeup(100000);
             esp_deep_sleep_start();
         }
-        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    default:
-        break;
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP ");
+        ESP_LOGI(TAG, "Login Success");
+        GOT_IP = true;
+
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
-    return ESP_OK;
 }
 
 static esp_err_t servePage_get_handler(httpd_req_t *req)
@@ -128,6 +142,7 @@ static const httpd_uri_t servePage = {
     .handler = servePage_get_handler,
     .user_ctx = NULL};
 
+void wifi_init_softap();
 static esp_err_t psw_ssid_get_handler(httpd_req_t *req)
 {
     char buf[128];
@@ -153,24 +168,37 @@ static esp_err_t psw_ssid_get_handler(httpd_req_t *req)
         }
 
         /* Log data received */
-        /* ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
+        ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
         ESP_LOGI(TAG, "%.*s", ret, buf);
-        ESP_LOGI(TAG, "===================================="); */
+        ESP_LOGI(TAG, "===================================="); 
+
         cJSON *root = cJSON_Parse(buf);
 
         sprintf(__SSID, "%s", cJSON_GetObjectItem(root, "ssid")->valuestring);
         sprintf(__PWD, "%s", cJSON_GetObjectItem(root, "pwd")->valuestring);
 
-        ESP_LOGI(TAG, "pwd: %s", __PWD);
-        ESP_LOGI(TAG, "ssid: %s", __SSID);
-
         remaining -= ret;
+
+        nvs_handle_t my_handle;
+    
+       esp_err_t err = nvs_open("storage", NVS_READWRITE, &my_handle);
+        if (err != ESP_OK) {
+            printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+        } else {
+            printf("Done\n");
+            err = nvs_set_str(my_handle, "BUFF", buf);
+            printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+            err = nvs_commit(my_handle);
+            printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // Close
+        nvs_close(my_handle);
+        }
     }
 
     // End response
-    httpd_resp_send_chunk(req, NULL, 0);
-    // esp_sleep_enable_timer_wakeup(100000);
-    // esp_deep_sleep_start();
+     httpd_resp_send_chunk(req, NULL, 0);
+    abort();
     return ESP_OK;
 }
 
@@ -202,47 +230,94 @@ static httpd_handle_t start_webserver(void)
 
 void wifi_init_softap()
 {
+    size_t required_size;
 
+    nvs_handle_t my_handle;
+    esp_err_t  err = nvs_open("storage", NVS_READWRITE, &my_handle);
+        if (err != ESP_OK) {
+            printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+        } else {
+            printf("Done\n");
+            nvs_get_str(my_handle, "BUFF", NULL, &required_size);
+            char* BUFF = malloc(required_size);
+            nvs_get_str(my_handle, "BUFF", BUFF, &required_size);
+            printf("%d", required_size);
+            if(required_size > 0)
+            {
+                switch (err) {
+                    case ESP_OK:
+                        printf("Done\n");
+                        cJSON *root = cJSON_Parse(BUFF);
+                        sprintf(__SSID, "%s", cJSON_GetObjectItem(root, "ssid")->valuestring);
+                        sprintf(__PWD, "%s", cJSON_GetObjectItem(root, "pwd")->valuestring);
+
+                        break;
+                    case ESP_ERR_NVS_NOT_FOUND:
+                        printf("The value is not initialized yet!\n");
+                        break;
+                    default :
+                        printf("Error (%s) reading!\n", esp_err_to_name(err));
+                }
+            }
+        }
     if (strlen(__PWD) && strlen(__SSID) != 0)
     {
-        ESP_LOGI(TAG, "WiFi %s ", __SSID);
-        ESP_LOGI(TAG, "PSW %s ", __PWD);
+    s_wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_netif_init());
 
-    //     ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-    //     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    //     esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-        
-    //     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    //     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-        
-    //      esp_event_handler_instance_t instance_any_id;
-    // esp_event_handler_instance_t instance_got_ip;
-    // ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-    //                                                     ESP_EVENT_ANY_ID,
-    //                                                     &event_handler,
-    //                                                     NULL,
-    //                                                     &instance_any_id));
-    // ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-    //                                                     IP_EVENT_STA_GOT_IP,
-    //                                                     &event_handler,
-    //                                                     NULL,
-    //                                                     &instance_got_ip));
-    
-    // ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
 
-    //     wifi_config_t wifi_config = {};
-    //     strcpy((char *)wifi_config.sta.ssid, __SSID);
-    //     strcpy((char *)wifi_config.sta.password, __PWD);
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "isolated",
+            .password = "noor@4321",
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
 
-    //     ESP_LOGI(TAG, "WiFi %s ", wifi_config.sta.ssid);
-    //     ESP_LOGI(TAG, "PSW %s ", wifi_config.sta.password);
-    //     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    //     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    //     ESP_ERROR_CHECK(esp_wifi_start());
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
 
-    //     xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
     }
     else
     {
